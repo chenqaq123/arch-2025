666869a60d9664bfc4de8ac585036e5e4237f803
cgx <2098583756@qq.com>
2025-03-19 18:49:39 +0800

    fix: 调试通过lab1

--- vsrc/src/pipeline/fetch/IF_ID_reg.sv
+++ vsrc/src/pipeline/fetch/IF_ID_reg.sv
@@ -14,19 +14,21 @@ module if_id_reg
     import pipes::*;(
     input logic clk, reset,
     input logic stallpc,
+    input logic stallM,
     input logic if_id_write,
     input fetch_data_t dataF_nxt,
     output fetch_data_t dataF
 );
     always_ff @(posedge clk) begin
+
         if(reset) begin
             dataF <= '0;
-        end else if(if_id_write) begin
-            dataF <= dataF_nxt;
-        end else if () begin 
-        end else begin
+        end else if (stallM) begin 
             dataF <= dataF;
+        end else begin
+            dataF <= dataF_nxt;
         end
+
     end
 
 endmodule
