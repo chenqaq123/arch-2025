54fec8b6e93556249128dc4e3926af60f0a39b27
cgx <2098583756@qq.com>
2025-04-09 16:14:20 +0800

    feat: 增加对不同alusrc的支持

--- vsrc/src/pipeline/execute/rd2_imm_mux.sv
+++ vsrc/src/pipeline/execute/rd2_imm_mux.sv
@@ -11,7 +11,8 @@
 module rd2_imm_mux
     import common::*;
     import pipes::*;(
-    input u64 rd2_from_register, imm_64,
+    input u64 rd2_from_register, imm_64, pc_add_4, pc_add_imm,
+    input u6 shamt,
     input ALUSRCType ALUSRC,
     output u64 rd2
 );
@@ -24,8 +25,17 @@ module rd2_imm_mux
             FromImm: begin
                 rd2 = imm_64;
             end
+            FromShamt: begin
+                rd2 = { {58{'0}}, {shamt[5:0]} };
+            end
             FromReg: begin
                 rd2 = rd2_from_register;
+            end
+            FromPcAdd4: begin
+                rd2 = pc_add_4;
+            end
+            FromPcAddImm: begin
+                rd2 = pc_add_imm;
             end
             default: begin
                 rd2 = '0;
